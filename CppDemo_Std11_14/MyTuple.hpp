a48d30bd5bda7de42e538923717fc56d0283ded5
cg <gg.wcg@qq.com>
2018-09-20 14:48:41 +0800

    MyTuple.hpp

---
+++ CppDemo_Std11_14/MyTuple.hpp
@@ -0,0 +1,32 @@
+#include "stdafx.h"
+using namespace std;
+/*
+Ä£°åµÝ¹é¼Ì³Ð
+*/
+template<typename... Values> class MyTuple;
+template<> class MyTuple<> {};
+
+template<typename Head, typename... Tail>
+class MyTuple<Head, Tail...>
+	:private MyTuple<Tail...>
+{
+	typedef MyTuple<Tail...> inherated;
+protected:
+	Head m_head;
+public:
+	MyTuple() {}
+	MyTuple(Head v, Tail... vtail)
+		:m_head(v), inherated(vtail...) {}
+
+	Head head() { return m_head; }
+	//auto head() -> decltype(m_head) { return m_head; }
+	inherated& tail() { return *this; }
+};
+
+void MyTupleTest() {
+	MyTuple<int, float, string> t(41, 6.3f, "Nice!");
+	cout << "sizeof MyTuple:"<< sizeof(t) << endl;
+	cout << t.head() << endl;
+	cout << t.tail().head() << endl;
+	cout << t.tail().tail().head() << endl;
+}
--- CppDemo_Std11_14/VariadicTemplates.hpp
+++ CppDemo_Std11_14/VariadicTemplates.hpp
@@ -3,8 +3,11 @@
 using namespace std;
 
 /*********  Variadic Templates(数量不定的模板参数)  **********/
+// 一、采用参数分解递归的方式
+//1.
 void print() {}
 
+//2. 与3相比，比较特化。
 template <typename T, typename... Types>
 void print(const T& firstArg, const Types&... args)
 {
@@ -12,6 +15,7 @@ void print(const T& firstArg, const Types&... args)
 	print(args...);
 }
 
+//3. 此模板函数未调用到。
 template <typename T, typename... Types>
 void print(const Types&... args)
 {
@@ -19,14 +23,40 @@ void print(const Types&... args)
 	print(args...);
 }
 
-template<typename T>
-using Vec = vector<T, allocator<T>>;
+/**********************/
+// 二、采用模板常量递归的方式
+// boost: util/printtuple.hpp
+template<int IDX, int MAX, typename... Args>
+struct PRINT_TUPLE
+{
+	static void print(ostream& os, const tuple<Args...>& t)
+	{
+		os << get<IDX>(t) << (IDX + 1 == MAX ? "" : ",");
+		PRINT_TUPLE<IDX + 1, MAX, Args...>::print(os, t);
+	}
+};
+// 终结条件: partial specialization to end the recursion
+template<int MAX, typename... Args>
+struct PRINT_TUPLE<MAX,MAX,Args...>
+{
+	static void print(ostream& os, const tuple<Args...>& t) { }
+};
 
-template<typename T>
-using Lst = list<T, allocator<T>>;
+// output perator for tuples
+template<typename... Args>
+ostream& operator<<(ostream& os, const tuple<Args...>& t)
+{
+	os << "[";
+	PRINT_TUPLE<0, sizeof...(Args), Args...>::print(os, t);
+	return os << "]";
+}
 
 void VariadicTemplates() {
 	//1. Variadic Templates
 	print(7.5, "hello中国", bitset<16>(377), 42);
+
+	auto t1 = make_tuple(17.5, "1hello中国", bitset<16>(377), 142);
+	cout << t1 << endl;
+
 	cout << endl;
 }
